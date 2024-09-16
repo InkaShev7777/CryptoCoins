32b8c15946b0369d209c72e05567662df3527592
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-09-16 11:41:24 +0300

    Create top movers item for home view

---
+++ CryptoCoin/App/CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinApp.swift
+//  CryptoCoin
+//
+//  Created by Ilya Schevchenko on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeVeiw()
+        }
+    }
+}
---
+++ CryptoCoin/Core/Home/View/HomeVeiw.swift
@@ -0,0 +1,23 @@
+//
+//  HomeVeiw.swift
+//  CryptoCoin
+//
+//  Created by Ilya Schevchenko on 16.09.2024.
+//
+
+import SwiftUI
+
+struct HomeVeiw: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+#Preview {
+    HomeVeiw()
+}
